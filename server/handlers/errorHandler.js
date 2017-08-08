9f756031e97a8700ba95240d4509994cbeeb7994
Abinash Patra <luffy.abinash@gmail.com>
2017-08-08 12:22:58 +0530

    Updating error handler for not getting fired

--- server/handlers/errorHandler.js
+++ server/handlers/errorHandler.js
@@ -1,7 +1,8 @@
 export default (app) => {
 
     if (process.env.NODE_ENV === 'dev') {
-        app.use((err, req, res) => {
+        // error handling middleware must take four param method signature
+        app.use((err, req, res, next) => {
             res.status(err.status || 500).send(err.message);
             // res.status(500).send({ error: err, message: err.message });
         });
